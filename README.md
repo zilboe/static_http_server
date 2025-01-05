c5bc455df42f4d75d444b8438e110681de53111b
zilboe <1149845493@qq.com>
2025-01-05 12:53:29 +0800

    static http server 0.1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+Static http server
+
+# Usage
+
+Currently, only the simplest http request is resolved and returned, and only the specified folder can be routed through the Future of the route, so as to query the current access path file.
+No other mechanism exists
+
---
+++ src/lib.rs
@@ -0,0 +1,353 @@
+use std::{
+    fs,
+    io::{Read, Write},
+    path::Path,
+    ptr::eq,
+};
+
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+
+use httparse::Request;
+struct HttpError {
+    message: String,
+    err_data: Vec<u8>,
+}
+
+impl From<io::Error> for HttpError {
+    fn from(error: io::Error) -> Self {
+        HttpError {
+            message: error.to_string(),
+            err_data: vec![],
+        }
+    }
+}
+
+#[warn(non_snake_case)]
+struct RequestConfig {
+    request_path: Option<String>,
+    keep_alive: bool,
+    encoder: bool,
+}
+
+impl RequestConfig {
+    fn new() -> Self {
+        RequestConfig {
+            request_path: None,
+            keep_alive: false,
+            encoder: false,
+        }
+    }
+
+    fn set_encode(&mut self, gzip_value: &str) {
+        self.encoder = gzip_value.contains("gzip");
+    }
+
+    fn set_keep_alive(&mut self, keep_alive_value: &str) {
+        self.keep_alive = eq(keep_alive_value, "true");
+    }
+
+    fn set_request_path_is_exist(&mut self, top_path: &str, uri_path: &str) -> bool {
+        let mut full_path: String = String::from(top_path);
+        full_path += uri_path;
+        if full_path.ends_with('/') {
+            full_path += "index.html"
+        }
+        let file_path: &Path = Path::new(&full_path);
+        if file_path.exists() {
+            self.request_path = Some(full_path);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn set_content_type(&mut self) -> Result<Vec<u8>, HttpError> {
+        let mut return_buffer: Vec<u8> = vec![];
+        let file_name = match &self.request_path {
+            Some(file) => file,
+            None => {
+                let err_message = "There is No File For Request";
+                return Err(HttpError {
+                    message: err_message.to_string(),
+                    err_data: "HTTP/1.1 404 OK\r\n\r\n".as_bytes().to_vec(),
+                });
+            }
+        };
+        match file_name.find('.') {
+            Some(pox) => {
+                let _file_type = match &file_name[pox..] {
+                    ".html" => "text/html",
+                    ".css" => "text/css",
+                    ".bmp" => "application/x-bmp",
+                    ".img" => "application/x-img",
+                    ".jpe" => "image/jpeg",
+                    ".jpeg" => "image/jpeg",
+                    ".jpg" => "image/jpeg",
+                    ".js" => "application/x-javascript",
+                    ".mp4" => "video/mpeg4",
+                    ".xml" => "	text/xml",
+                    ".xquery" => "text/xml",
+                    ".xsl" => "text/xml",
+                    _ => "application/octet-stream",
+                };
+
+                let file_type = format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\n", _file_type);
+                return_buffer.extend_from_slice(file_type.as_bytes());
+            }
+            None => {
+                let err_message = format!("The File Name {} Error,No Dot", file_name);
+                return Err(HttpError {
+                    message: err_message,
+                    err_data: "HTTP/1.1 404 OK\r\n\r\n".as_bytes().to_vec(),
+                });
+            }
+        }
+        Ok(return_buffer)
+    }
+
+    fn sl_http_fill_file_buffer(&mut self) -> Result<Vec<u8>, HttpError> {
+        let mut file_buff: Vec<u8> = Vec::new();
+        let file_name = match &self.request_path {
+            Some(files) => files,
+            None => {
+                let err_message = "There Is No File For Request";
+                return Err(HttpError {
+                    message: err_message.to_string(),
+                    err_data: vec![],
+                });
+            }
+        };
+        let files = fs::OpenOptions::new().read(true).open(file_name);
+        match files {
+            Ok(mut files) => match files.read_to_end(&mut file_buff) {
+                Ok(_) => {
+                    if self.encoder {
+                        let mut gzip = GzEncoder::new(Vec::new(), Compression::default());
+                        let gzip_buff_result = match gzip.write_all(&file_buff) {
+                            Ok(_) => match gzip.finish() {
+                                Ok(buffer) => buffer,
+                                Err(_) => {
+                                    let err_message =
+                                        format!("The File ({}) Encoder Gzip Error", file_name);
+                                    return Err(HttpError {
+                                        message: err_message,
+                                        err_data: vec![],
+                                    });
+                                }
+                            },
+                            Err(_) => {
+                                let err_message =
+                                    format!("The File ({}) Encoder Gzip Error", file_name);
+                                return Err(HttpError {
+                                    message: err_message,
+                                    err_data: vec![],
+                                });
+                            }
+                        };
+                        let mut file_content =
+                            format!("Content-Length: {}\r\n\r\n", gzip_buff_result.len())
+                                .as_bytes()
+                                .to_vec();
+                        file_content.extend(gzip_buff_result);
+                        Ok(file_content)
+                    } else {
+                        let mut file_content =
+                            format!("Content-Length: {}\r\n\r\n", file_buff.len())
+                                .as_bytes()
+                                .to_vec();
+                        file_content.extend(file_buff);
+                        Ok(file_content)
+                    }
+                }
+                Err(_) => {
+                    let err_message = format!("Can't Read The File ({})", file_name);
+                    Err(HttpError {
+                        message: err_message,
+                        err_data: file_buff,
+                    })
+                }
+            },
+            Err(_) => {
+                let err_message = format!("Can't Open The File ({})", file_name);
+                Err(HttpError {
+                    message: err_message,
+                    err_data: file_buff,
+                })
+            }
+        }
+    }
+
+    fn static_http_process_request(&mut self, top_path: &str, recv_request: &[u8]) -> Vec<u8> {
+        let mut send_buffer: Vec<u8> = Vec::new();
+        let mut header = [httparse::EMPTY_HEADER; 64];
+        let mut request = Request::new(&mut header);
+        request.parse(recv_request).unwrap();
+        if !self.set_request_path_is_exist(top_path, request.path.unwrap()) {
+            send_buffer.extend_from_slice(b"HTTP/1.1 404 OK\r\n\r\n");
+            return send_buffer;
+        }
+
+        let response_code = match self.set_content_type() {
+            Ok(response) => response,
+            Err(e) => {
+                println!("{}", e.message);
+                return e.err_data;
+            }
+        };
+
+        send_buffer.extend(response_code);
+
+        for header_item in request.headers {
+            if header_item.name.eq_ignore_ascii_case("Accept-Encoding") {
+                self.set_encode(std::str::from_utf8(header_item.value).unwrap());
+            }
+            if header_item.name.eq_ignore_ascii_case("keep-alive") {
+                self.set_keep_alive(std::str::from_utf8(header_item.value).unwrap());
+            }
+        }
+        if self.encoder {
+            send_buffer.extend_from_slice(b"Content-Encoding: gzip\r\n");
+        }
+
+        let file_content = match self.sl_http_fill_file_buffer() {
+            Ok(file) => file,
+            Err(e) => {
+                println!("{}", e.message);
+                e.err_data
+            }
+        };
+        send_buffer.extend(file_content);
+        send_buffer
+    }
+}
+
+#[warn(non_snake_case)]
+pub struct StaticHttp<'a> {
+    listen: Option<tokio::net::TcpListener>,
+
+    paths: Option<&'a str>,
+    // #[warn(non_snake_case)]
+    // #[warn(dead_code)]
+    //keep_alive_time: Option<usize>,
+}
+
+impl Default for StaticHttp<'static> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'a> StaticHttp<'static> {
+    pub fn new() -> Self {
+        StaticHttp {
+            paths: None,
+            listen: None,
+            // keep_alive_time: None,
+        }
+    }
+
+    pub async fn bind(mut self, ip_port: &str) -> Result<Self, ()> {
+        let tcp_listen = match TcpListener::bind(ip_port).await {
+            Ok(tcp_listen) => tcp_listen,
+            Err(_) => {
+                let err_message = format!("Can't Bind The Addr {}", ip_port);
+                println!("{}", err_message);
+                return Err(());
+            }
+        };
+        self.listen = Some(tcp_listen);
+        Ok(self)
+    }
+
+    pub async fn run(self, start: bool) -> Result<(), ()> {
+        if start {
+            if self.paths.is_none() {
+                println!("Please Fill in The Route Path \".route()\"");
+                return Err(());
+            }
+            let listener = match self.listen {
+                Some(listener) => listener,
+                None => {
+                    let err_message = "There is No Listener Used";
+                    println!("{}", err_message);
+                    return Err(());
+                }
+            };
+            println!("The Server Start Listen!");
+            let top_path = self.paths.unwrap();
+            loop {
+                match listener.accept().await {
+                    Ok((socket, _)) => {
+                        tokio::spawn(async move {
+                            static_http_handle_process(top_path, socket).await;
+                        });
+                    }
+                    Err(_) => {
+                        let err_message = "The Listener Accept Error,Please Check The Addr Port";
+                        println!("{}", err_message);
+                        return Err(());
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn route(mut self, route_path: &'static str) -> Result<Self, ()> {
+        if !route_path.is_empty() {
+            self.paths = Some(route_path);
+        } else {
+            let err_message = "The Route Path Is Null";
+            println!("{}", err_message);
+            return Err(());
+        }
+        Ok(self)
+    }
+}
+
+async fn static_http_handle_process(top_path: &str, mut stream: TcpStream) {
+    let mut recv_request_buffer: [u8; 2048] = [0; 2048];
+    let mut request_config = RequestConfig::new();
+    let send_buffer = match stream.read(&mut recv_request_buffer).await {
+        Ok(recv_size) => {
+            let capture_request_buffer = &recv_request_buffer[..recv_size];
+            request_config.static_http_process_request(top_path, capture_request_buffer)
+        }
+        Err(_) => {
+            let err_message = format!("The ({:?}) Recv Error", stream);
+            println!("{}", err_message);
+            return;
+        }
+    };
+    match stream.write_all(&send_buffer).await {
+        Ok(()) => {}
+        Err(_) => {
+            println!("The Stream Send Error")
+        }
+    };
+    match stream.shutdown().await {
+        Ok(()) => {}
+        Err(_) => {
+            println!("The Stream Shutdown Error")
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn http_server() {
+        StaticHttp::new()
+            .bind("127.0.0.1:789").await.unwrap()
+            .route("C:\\Users\\Desktop\\website").unwrap()
+            .run(true).await.unwrap();
+    }
+}

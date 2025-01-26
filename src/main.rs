056fbf570072801c0738efa327d79172f2ad74f0
zilboe <1149845493@qq.com>
2025-01-26 20:16:11 +0800

    Completion program

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use config::Config;
+use http::HttpServer;
+
+#[tokio::main]
+async fn main() {
+    let config = Config::read_config().unwrap();
+    HttpServer::new()
+        .bind(&config.ip_port).await.unwrap()
+        .route(&config.web_page).unwrap()
+        .set_keep_alive(config.keepalive_timeout)
+        .run().await;
+}
